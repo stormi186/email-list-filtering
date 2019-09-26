a51d199cc25761eeccd09394ea2533d88fe60bc7
stormi186 <jasna.misimovic@gmail.com>
2019-09-26 11:28:52 +0200

    Updated README.md file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,76 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let content1, content2, content;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const ImportFromFileBodyComponent = () => {
+  let fileReader;
+
+  const handleFileRead1 = (e) => {
+      content1 = fileReader.result.split("\n");
+  };
+
+  const handleFileRead2 = (e) => {
+    content2 = fileReader.result.split("\n");
+    content = content1.filter(f => !content2.includes(f));
+    let newList = content.map((item) => item + "\n");
+    const superBuffer = new Blob(newList, {type : 'text/html'});
+    let objUrl = window.URL.createObjectURL(superBuffer);
+    let anchor = document.createElement('a');
+    anchor.href = objUrl;
+    anchor.setAttribute('download', 'newlist.txt');
+    anchor.click();
+};
+
+  const handleFileChosen1 = (file) => {
+      fileReader = new FileReader();
+      fileReader.onloadend = handleFileRead1;
+      fileReader.readAsText(file);
+  };
+
+  const handleFileChosen2 = (file) => {
+    fileReader = new FileReader();
+    fileReader.onloadend = handleFileRead2;
+    fileReader.readAsText(file);
+};
+
+  return (
+  <div>
+    <div className='upload-list1'>
+    <h1>Main email list in .txt format</h1>
+      <input type='file'
+             id='list1'
+             className='input-list1'
+             accept='.txt'
+             onChange={e => handleFileChosen1(e.target.files[0])}
+      />
+    </div>
+    <div className='upload-list2'>
+    <h1>List of emails to be deleted in .txt format</h1>
+      <input type='file'
+             id='list2'
+             className='input-list2'
+             accept='.txt'
+             onChange={e => handleFileChosen2(e.target.files[0])}
+      />
+    </div>     
+  </div>);
+
+};
+
+const App = () => {
+
+  return (
+    <div>
+      <ImportFromFileBodyComponent />
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
+
+
